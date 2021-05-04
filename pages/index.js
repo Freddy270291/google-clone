a1d0457ffee0a39849aa07d536d74b6b084086f6
Federico Giovannini <giovannini.federico@hotmail.it>
2021-05-04 20:45:51 +0200

    Navbar completed

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,7 @@
 import Head from "next/head";
 import Avatar from "../components/Avatar";
+import { MicrophoneIcon, ViewGridIcon } from "@heroicons/react/solid";
+import Image from "next/image";
 
 export default function Home() {
   return (
@@ -16,14 +18,22 @@ export default function Home() {
         <div className='flex space-x-4 items-center'>
           <p className='link'>GMail</p>
           <p className='link'>Images</p>
-          {/* Icon */}
+
+          <ViewGridIcon className='h-10 w-10 p-2 rounded-full hover:bg-gray-100 cursor-pointer' />
           <Avatar url='https://media-exp1.licdn.com/dms/image/C5603AQF_ETUBUqItmA/profile-displayphoto-shrink_200_200/0/1555336290907?e=1624492800&v=beta&t=RA2GW98oBSqkX8nU66XT88H7GLGv4zyOh4Z1WUpruzQ' />
         </div>
       </header>
       {/* Body */}
+      <form>
+        <Image
+          src='https://www.google.co.uk/images/branding/googlelogo/2x/googlelogo_color_272x92dp.png'
+          height={100}
+          width={300}
+        />
+      </form>
       {/* Footer */}
     </div>
   );
 }
 
-// 44:00
+//52:00
